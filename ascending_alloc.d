337ed89a83dd91c182e79e1996563b1010229888
Alexandru Jercaianu <alex.jercaianu@gmail.com>
2017-11-20 23:23:45 +0200

    refactor + tests

--- ascending_alloc.d
+++ ascending_alloc.d
@@ -10,9 +10,6 @@ size_t roundUpToMultipleOf(size_t s, uint base)
 
 struct AscendingAllocator
 {
-    import std.bitmanip : BitArray;
-    import std.experimental.allocator.mallocator : Mallocator;
-
     size_t numPages;
     enum size_t pageSize = 4096;
     void* data;
@@ -20,85 +17,62 @@ struct AscendingAllocator
     size_t pagesUsed;
     bool valid;
 
-    // Receives as a parameter the size in pages for each arena 
-    version(Posix)
+    this(size_t pages)
     {
-        this(size_t pages)
+        import std.exception : enforce;
+
+        valid = true;
+        numPages = pages;
+        version(Posix)
         {
             import core.sys.posix.sys.mman : mmap, MAP_ANON, PROT_READ,
                    PROT_WRITE, PROT_NONE, MAP_PRIVATE, MAP_FAILED;
-            import std.exception : enforce;
-
-            valid = true;
-            numPages = pages;
             data = mmap(null, pageSize * pages, PROT_NONE, MAP_PRIVATE | MAP_ANON, -1, 0);
             enforce(data != MAP_FAILED, "Failed to mmap memory");
-            offset = data;
         }
-    }
-    else version(Windows)
-    {
-        this(size_t pages)
+        else version(Windows)
         {
             import core.sys.windows.windows : VirtualAlloc, PAGE_NOACCESS,
                    MEM_COMMIT, MEM_RESERVE;
-            import std.exception : enforce;
-
-            valid = true;
-            numPages = pages;
             data = VirtualAlloc(null, pageSize * pages, MEM_COMMIT | MEM_RESERVE, PAGE_NOACCESS);
             enforce(data != null, "Failed to VirtualAlloc memory");
-            offset = data;
         }
+        offset = data;
+
     }
 
     // Allocate multiple of page size, by changing page protection inside the arena
-    version(Posix)
+    void[] allocate(size_t n)
     {
-        void[] allocate(size_t n)
+        import std.exception : enforce;
+
+        // If the requested size exceeds available space in current arena,
+        // create new arena
+        size_t goodSize = goodAllocSize(n);
+        if (offset - data > numPages * pageSize - goodSize)
+            return null;
+
+        void* result = offset;
+        offset += goodSize;
+        pagesUsed += goodSize / pageSize;
+
+        // Give memory back just by changing page protection
+        version(Posix)
         {
             import core.sys.posix.sys.mman : mmap, mprotect,
                    MAP_PRIVATE, MAP_ANON, MAP_FAILED, PROT_NONE, PROT_WRITE, PROT_READ;
-            import std.exception : enforce;
-
-            // If the requested size exceeds available space in current arena,
-            // create new arena
-            size_t goodSize = goodAllocSize(n);
-            if (offset - data > numPages * pageSize - goodSize)
-                return null;
-
-            void* result = offset;
-            offset += goodSize;
-            pagesUsed += goodSize / pageSize;
-
-            // Give memory back just by changing page protection
             auto ret = mprotect(result, goodSize, PROT_WRITE | PROT_READ);
             enforce(ret == 0, "Failed to allocate memory, mprotect failure"); 
-
-            return cast(void[]) result[0 .. n];
         }
-    }
-    else version(Windows)
-    {
-        void[] allocate(size_t n)
+        else version(Windows)
         {
             import core.sys.windows.windows : VirtualProtect, PAGE_READWRITE;
-            import std.exception : enforce;
-
             uint oldProtect;
-            size_t goodSize = goodAllocSize(n);
-            if (offset - data > numPages * pageSize - goodSize)
-                return null;
-
-            void *result = offset;
-            offset += goodSize;
-            pagesUsed += goodSize / pageSize;
-
             auto ret = VirtualProtect(result, goodSize, PAGE_READWRITE, &oldProtect);
             enforce(ret != 0, "Failed to allocate memory, VirtualProtect failure");
-
-            return cast(void[]) result[0 .. n];
         }
+
+        return cast(void[]) result[0 .. n];
     }
 
     size_t goodAllocSize(size_t n)
@@ -111,8 +85,7 @@ struct AscendingAllocator
     {
         bool deallocate(void[] buf)
         {
-            import core.sys.posix.sys.mman : posix_madvise, POSIX_MADV_DONTNEED, mprotect, PROT_NONE;
-            import core.sys.posix.sys.mman : munmap, mprotect, PROT_NONE;
+            import core.sys.posix.sys.mman : posix_madvise, POSIX_MADV_DONTNEED, mprotect, PROT_NONE, munmap;
             import std.exception : enforce;
 
             size_t goodSize = goodAllocSize(buf.length);
@@ -165,34 +138,93 @@ struct AscendingAllocator
         return buf.ptr >= data && buf.ptr < buf.ptr + numPages * pageSize;
     }
 
-    version(Posix)
+    void invalidate()
     {
-        void invalidate()
-        {
-            import core.sys.posix.sys.mman : munmap;
-            valid = false;
-            if (pagesUsed == 0) { 
+        valid = false;
+        if (pagesUsed == 0) { 
+            version(Posix)
+            {
+                import core.sys.posix.sys.mman : munmap;
                 munmap(data, numPages * pageSize);
-                data = null;
             }
-        }
-    }
-    else version(Windows)
-    {
-        void invalidate()
-        {
-            import core.sys.windows.windows : VirtualFree, MEM_RELEASE;
-            valid = false;
-            if (pagesUsed == 0) {
+            else version(Windows)
+            {
+                import core.sys.windows.windows : VirtualFree, MEM_RELEASE;
                 VirtualFree(data, 0, MEM_RELEASE);
-                data = null;
             }
+            data = null;
         }
     }
 
     size_t getAvailableSize()
     {
         return numPages * pageSize + data - offset;
+    }
+
+    bool expand(ref void[] b, size_t delta)
+    {
+        import std.exception : enforce;
+
+        if (!delta) return true;
+        if (!b.ptr) return false;
+
+        size_t goodSize = goodAllocSize(b.length);
+        if (b.ptr + goodSize != offset)
+            return false;
+
+        size_t extraPages = 0;
+        size_t bytesLeftOnPage = goodSize - b.length;
+
+        if (delta > bytesLeftOnPage)
+        {
+            extraPages = goodAllocSize(delta - bytesLeftOnPage) / pageSize;
+        }
+        else
+        {
+            b = cast(void[]) b.ptr[0 .. b.length + delta];
+            return true;
+        }
+
+        if (extraPages > numPages)
+            return false;
+
+        if (offset - data > pageSize * (numPages - extraPages))
+            return false;
+
+        version(Posix)
+        {
+            import core.sys.posix.sys.mman : mprotect, PROT_READ, PROT_WRITE;
+            auto ret = mprotect(offset, extraPages * pageSize, PROT_READ | PROT_WRITE);
+            enforce(ret == 0, "Failed to expand, mprotect failure");
+        }
+        else version(Windows)
+        {
+            import core.sys.windows.windows : VirtualProtect, PAGE_READWRITE;
+            uint oldProtect;
+            auto ret = VirtualProtect(offset, extraPages * pageSize, PAGE_READWRITE, &oldProtect);
+            enforce(ret != 0, "Failed to expand, VirtualProtect failure");
+        }
+
+        pagesUsed += extraPages;
+        offset += extraPages * pageSize;
+        b = cast(void[]) b.ptr[0 .. b.length + delta];
+        return true;
+    }
+
+    bool reallocate(ref void[] b, size_t newSize)
+    {
+        if (!newSize) return deallocate(b);
+        if (!b) return true;
+
+        if (newSize >= b.length && expand(b, newSize - b.length))
+            return true;
+        
+        void[] newB = allocate(newSize);
+        if (newB.length <= b.length) newB[] = b[0 .. newB.length];
+        else newB[0 .. b.length] = b[];
+        deallocate(b);
+        b = newB;
+        return true;
     }
 }
 
@@ -239,13 +271,75 @@ struct AscendingAllocator
 
 @system unittest
 {
-    size_t numPages = 4194304;
+    size_t numPages = 26214;
     AscendingAllocator a = AscendingAllocator(numPages);
     for (int i = 0; i < numPages; i++) {
         void[] buf = a.allocate(1);
+        assert(buf.length == 1);
         *cast(ubyte*)buf.ptr = 10;
+        assert(*cast(ubyte*)buf.ptr == 10);
         a.deallocate(buf);
     }
+
+    assert(!a.allocate(1));
+    assert(a.getAvailableSize() == 0);
+    a.invalidate();
+    assert(!a.data);
+}
+
+@system unittest
+{
+    size_t numPages = 5;
+    enum pageSize = 4096;
+    AscendingAllocator a = AscendingAllocator(numPages);
+
+    void[] b1 = a.allocate(2048);
+    assert(b1.length == 2048);
+    
+    void[] b2 = a.allocate(2048);
+    assert(!a.expand(b1, 1));
+    assert(a.expand(b1, 0));
+
+    assert(a.expand(b2, 2048));
+    assert(b2.length == pageSize);
+    assert(a.getAvailableSize() == pageSize * 3);
+
+    void[] b3 = a.allocate(2048);
+    assert(a.reallocate(b1, b1.length));
+    assert(a.reallocate(b2, b2.length));
+    assert(a.reallocate(b3, b3.length));
+
+    assert(b3.length == 2048);
+    assert(a.expand(b3, 1000));
+    assert(a.expand(b3, 0));
+    assert(b3.length == 3048);
+    assert(a.expand(b3, 1047));
+    assert(a.expand(b3, 0));
+    assert(b3.length == 4095);
+    assert(a.expand(b3, 100));
+    assert(a.expand(b3, 0));
+    assert(a.getAvailableSize() == pageSize);
+    assert(b3.length == 4195);
+
+    assert(a.reallocate(b1, b1.length));
+    assert(a.reallocate(b2, b2.length));
+    assert(a.reallocate(b3, b3.length));
+    
+    assert(a.reallocate(b3, 2 * pageSize));
+    assert(a.reallocate(b1, pageSize - 1));
+    assert(a.expand(b1, 1));
+    assert(!a.expand(b1, 1));
+
+    a.invalidate();
+    a.deallocate(b1);
+    a.deallocate(b2);
+    a.deallocate(b3);
+    assert(!a.data);
+}
+
+@system unittest
+{
+
 }
 
 void main()
